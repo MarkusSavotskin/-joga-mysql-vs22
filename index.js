6090edc4ee21dd8701a6d80f168704bc5cd3e724
Markus Savotškin <markus.savotskin@voco.ee>
2023-09-07 10:25:38 +0300

    Created index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+// application packages
+const express = require('express')
+const app = express()
+
+const path = require('path')
+// add template engine
+const hbs = require('express-handlebars');
+// setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+}))
+
+const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// create database connection
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'mydb'
+})
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to mydb");
+})
+
+app.listen(3000, () => {
+    console.log('App is started at http://localhost:3000')
+})
