da1a8c1d49e08468712e0de508d590c8b415ba1d
Markus Savotškin <markus.savotskin@voco.ee>
2023-09-14 09:28:57 +0300

    Display all articles using the article model

--- controllers/article.js
+++ controllers/article.js
@@ -1,15 +1,19 @@
 const con = require('../utils/db')
+const Article = require('../models/article.model')
 
 // show all articles index page
 const getAllArticles = (req, res) => {
-    let query = "SELECT * FROM article";
-    let articles = []
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        articles = result
-        res.render('index', {
-            articles: articles
-        })
+    Article.getAll((err, data) => {
+        if (err) {
+            res.status(500).send({
+                message : err.message || 'An error occurred retrieving articles data'
+            })
+        } else {
+            console.log(data)
+            res.render('index', {
+                articles: data
+            })
+        }
     })
 };
 //show article by this slug
